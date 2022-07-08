9857a6a5f0f1166dd03720bee87860fcfa997265
ChiragSankhla1902 <53487812+ChiragSankhla1902@users.noreply.github.com>
2022-07-08 12:10:16 +0530

    Added Multiple Lift Task

--- js/main.js
+++ js/main.js
@@ -5,71 +5,99 @@ let floorOrder=[]
 window.scrollTo(0,0);
 function CreateTheWorld(){
     let floor=parseInt(document.getElementById('NumberOfFloor').value);
-    let floorContainer = document.createElement("div");
-    floorContainer.classList.add("Floors")
- 
-    for(let i=floor-1;i>=0;--i)
+    let lifts = parseInt(document.getElementById('NumberOfLift').value);
+    if(floor>0 && lifts>0)
     {
-        let floor1=        `<div class="FloorContainer">
-        <div class="ButtonContainer">
-           ${i!==floor-1 ?  `<button class="up-button ${i}" id="${i}" onclick="handleclick(id)"> up </button>`:``}
-           ${i!==0 ?  `<button class="down-button ${i}" id="${i}" onclick="handleclick(id)"> down </button>`:``}
-        </div>
-        <div class="FloorDetail">
-            <div class="Line"></div><h3>Floor ${i}</h3>
-        </div>
-    </div>`
-    floorContainer.innerHTML+= floor1;
-       
-    }
-    let elevator=`
-    <div id="Lift" class="SingleLift">
-        <div class="LeftDoor" ></div>
-        <div class="RightDoor" ></div>
-    </div>`
-    floorContainer.innerHTML+=elevator
-    Container[0].innerHTML=''
-    Container[0].prepend(floorContainer)
-
-    // let elevatorContainer =document.createElement("div");
+        let floorContainer = document.createElement("div");
+        floorContainer.classList.add("Floors")
+     
+        for(let i=floor-1;i>=0;--i)
+        {
+            let floor1=        `<div class="FloorContainer">
+            <div class="ButtonContainer">
+               ${i!==floor-1 ?  `<button class="up-button ${i}" id="${i}" onclick="handleclick(id)"> up </button>`:``}
+               ${i!==0 ?  `<button class="down-button ${i}" id="${i}" onclick="handleclick(id)"> down </button>`:``}
+            </div>
+            <div class="FloorDetail">
+                <div class="Line"></div><h3>Floor ${i}</h3>
+            </div>
+        </div>`
+        floorContainer.innerHTML+= floor1;   
+        }
+        let ElevatorContainer = document.createElement("div");
+        ElevatorContainer.classList.add("ElevatorContainer")
+        for(let i=0;i<lifts;i++){
+            let elevator=`
+            <div id="Lift" class="SingleLift" style="left:${((i+1)*15)+"%"}">
+                <div class="LeftDoor" ></div>
+                <div class="RightDoor" ></div>
+            </div>`
+            floorContainer.innerHTML+=elevator
+        }
     
-    elevatorContainer.innerHTML=elevator
-//   Container[0].append(elevatorContainer)
+    
+        // floorContainer.innerHTML+=ElevatorContainer
+        // console.log(floorContainer)
+        
+        Container[0].innerHTML=''
+        Container[0].prepend(floorContainer)
+        // Container[0].prepend(ElevatorContainer)
+    
+        // let elevatorContainer =document.createElement("div");
+        
+        // elevatorContainer.innerHTML=elevator
+    //   Container[0].append(elevatorContainer)
+    }
+    else{
+        alert('Enter Valid number')
+    }
 }
 
-function MoveLift(id){
-    let Lift=document.getElementById('Lift')
-    if(!Lift.classList.contains("busy"))
+function MoveLift(id,FreeLift){
+    if(!FreeLift.classList.contains("busy"))
     {
-        Lift.style.transform=`translateY(-${(280 *(parseInt(id)))}px)`
-        Lift.classList.add('busy')
-        setTimeout( ()=>{Lift.children[0].classList.add('OpenLeftDoor'); Lift.children[1].classList.add('OpenRightDoor')},4500) 
-        setTimeout( ()=>{Lift.children[0].classList.remove('OpenLeftDoor'); Lift.children[1].classList.remove('OpenRightDoor')},8500) 
-        setTimeout( ()=>{Lift.classList.remove('busy')},11000) 
+        FreeLift.style.transform=`translateY(-${(272 *(parseInt(id)))}px)`
+        FreeLift.classList.add('busy')
+        setTimeout( ()=>{FreeLift.children[0].classList.add('OpenLeftDoor'); FreeLift.children[1].classList.add('OpenRightDoor')},4500) 
+        setTimeout( ()=>{FreeLift.children[0].classList.remove('OpenLeftDoor'); FreeLift.children[1].classList.remove('OpenRightDoor')},8500) 
+        setTimeout( ()=>{FreeLift.classList.remove('busy')},11000) 
     }
 }
 
 var flag=1;
 
 function handleclick(id){
-    let Lift=document.getElementById('Lift')
-    if(!Lift.classList.contains("busy")&& flag){
-        MoveLift(id)
-        flag=0;
-    }
-    else{
+    let Alllift=document.querySelectorAll('#Lift')
+    let FreeLift=FreeLiftdiV(Alllift)
+
         floorOrder.push(id)
-        console.log('FLoor Queued',id,floorOrder);
-    }
 
         setInterval(() => {
-        if(floorOrder.length && !Lift.classList.contains("busy") )
+        if(floorOrder.length)
         {
-            console.log('Lift going to',floorOrder[0],floorOrder)
-            MoveLift(floorOrder.shift())
-            console.log(floorOrder)
-            
+            FreeLift=FreeLiftdiV(Alllift)
+            if(!FreeLift.classList.contains("busy")){
+                // console.log('Lift going to',floorOrder[0],floorOrder)
+                MoveLift(floorOrder.shift(),FreeLift)
+                // console.log("After",floorOrder)
+            }
+            else{
+                console.log('No lIft is Free')
+            }
+
         }
-       }, 5000);
+
+       }, 2000);
  
+}
+
+function FreeLiftdiV(Alllift)
+{
+    let liftsArray = Array.from(Alllift)
+    for( i = 0 ; i < liftsArray.length ; i++){
+        if(!liftsArray[i].classList.contains("busy")){
+          return liftsArray[i];
+        }
+    } 
+    return 0;
 }
